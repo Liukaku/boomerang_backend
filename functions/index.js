e996978661221b46b45775efc9275fe77d964693
Liukaku <46352428+Liukaku@users.noreply.github.com>
2021-05-01 22:25:54 +0100

    added endpoint for image upload

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,15 @@ const functions = require("firebase-functions");
 const fbadmin = require('firebase-admin')
 
 const { admin, db } = require('./util/admin')
-const { getCard, checkCards } = require('./handlers/calls')
-const { newCard } = require('./handlers/calls')
+const { getCard, checkCards, imageUpload, newCard } = require('./handlers/calls')
 
 const app = require('express')();
 const { response } = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+//TODO:
+//Allow the creation of a secret password for each one
+//so that you can edit it in the future if you wanted
 
 //create new business card
 app.post('/card', newCard)
@@ -22,6 +18,10 @@ app.post('/card', newCard)
 //get existing by URL
 app.get('/card/:urlname', getCard)
 
+//upload an image
+app.post('/card/image', imageUpload)
+
+//debugging endpoint for confirming if a url is in use
 app.get('/check/:urlname', checkCards)
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
