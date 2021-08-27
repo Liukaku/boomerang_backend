35f699d7c44c0bd004d47a1a7bb13ff693557da9
Liukaku <46352428+Liukaku@users.noreply.github.com>
2021-08-27 11:08:36 +0100

    gcloud link test

--- functions/index.js
+++ functions/index.js
@@ -27,4 +27,15 @@ app.get('/check/:urlname', checkCards)
 //used to change the colour for the card background and deletes the background image
 app.put('/card/background/:urlname', changeCol)
 
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin.firestore().collection('messages').add({original: original});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
 exports.api = functions.region('europe-west1').https.onRequest(app);
